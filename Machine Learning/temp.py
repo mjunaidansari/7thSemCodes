334e0c17dca6b10c092d049142a736348f9aa0d8
Junaid Ansari <junaidshahid.ansari@gmail.com>
2024-07-29 23:02:04 +0530

    Modify Linear Regression program to directly plot the predicted values of X.

--- Machine Learning/temp.py
+++ Machine Learning/temp.py
@@ -10,6 +10,11 @@ data = {
     'y': [94, 73, 59, 80, 93, 85, 66, 79, 77, 91]
 }
 
+# data = {
+#     'x': [5, 10, 15, 20],
+#     'y': [25, 35, 40, 50]
+# }
+
 df = pd.DataFrame(data)
 
 X = df[['x']]
@@ -24,13 +29,14 @@ print("Intercept: ", model.intercept_)
 print("Slope: ", model.coef_[0])
 
 # predict for a new value of x
-new_x = pd.DataFrame({'x': [7]})
+new_x = np.array([[7]])
 predicted_y = model.predict(new_x)
-print(f"\nPredicted y for x: {new_x['x'][0]} is {predicted_y[0]}")
+print(f"Predicted y for x: {new_x[0][0]} is {predicted_y[0]}")
 
-# plot the graph
+# # plot the graph
 plt.scatter(X, y, color='blue', label='Training Data')
-plt.plot(X, model.predict(X), color='red', label='Regression Line')
+# plt.plot(X, [(model.intercept_ + model.coef_[0]*float(x)) for x in X.values], color='red', label='predicted')
+plt.plot(X, model.predict(X), color='red', label='predicted')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Linear Regression')

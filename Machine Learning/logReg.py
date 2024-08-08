90e7745c95a5b8c63986e24df42fc95b409da755
Junaid Ansari <junaidshahid.ansari@gmail.com>
2024-08-08 23:43:39 +0530

    Modify dataset for Logistic Regression

--- Machine Learning/logReg.py
+++ Machine Learning/logReg.py
@@ -4,16 +4,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.linear_model import LogisticRegression
 
-# data = {
-#     'x1': [14.5, 8.5],
-#     'x2': [12.5, 4.5],
-#     'y': [1, 0]
-# }
-
 data = {
     'x1': [14.5, 8.5, 15.0, 9.0, 13.0, 10.5, 16.0, 7.5, 12.0, 11.0],
     'x2': [12.5, 4.5, 13.0, 5.0, 11.5, 6.0, 14.0, 3.5, 10.0, 7.0],
-    'y': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]  # Binary target variable
+    'y': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
 }
 
 # Convert dictionary to DataFrame
